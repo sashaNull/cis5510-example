691100af2234937bac8811f33883e00c3600ea63
sashaNull <nguyendinhcuong0512@gmail.com>
2024-04-25 12:47:24 -0400

    Finalize Chall 7

--- software-security/level-7/softsec_level7.c
+++ software-security/level-7/softsec_level7.c
@@ -1,14 +1,13 @@
-// Compiled with DEP enabled.
-// It's OK if your program crashes after the user exits the shell.
 // gcc ./softsec_level7.c -o softsec_level7 -fno-PIE -no-pie -fno-stack-protector
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-void auth(int pennkey, char *name, char *email, char *research, int course_code, char *cmd, bool pwnnsylvanian, char *fav_ctf_category) {
+void auth(int pennkey, char *name, char *email, char *research, int course_code,
+          char *cmd, bool pwnnsylvanian, char *fav_ctf_category) {
   printf("Pwnnsylvanian? %s\n", pwnnsylvanian ? "true" : "false");
   printf("Pennkey: %d\n", pennkey);
   printf("Name: %s\n", name);
@@ -18,12 +17,9 @@ void auth(int pennkey, char *name, char *email, char *research, int course_code,
   printf("Favourite CTF category: %s\n", fav_ctf_category);
   printf("Executing command......\n");
   sleep(1);
-  if (pennkey == 0x1337 && pwnnsylvanian)
-  {
+  if (pennkey == 0x1337 && pwnnsylvanian) {
     system(cmd);
-  }
-  else
-  {
+  } else {
     printf("Sorry, only Sebastian has the permission to execute commands!\n");
   }
 }
@@ -32,13 +28,14 @@ void read_buffer() {
   char buf[8];
   printf("READING.............\n");
   sleep(1);
-  read(0, buf, 0x13);
+  read(0, buf, 0x12);
   printf("Dest address read: %p!\n", &buf);
 }
 
 int main() {
   setuid(0);
-  auth(0x1337, "sebastian", "sebastian.angel@cis.upenn.edu", "Cybersecurity", 5510, "echo CIS5510 is awesome!", true, "pwn, rev");
+  auth(0x1337, "sebastian", "sebastian.angel@cis.upenn.edu", "Cybersecurity",
+       5510, "echo CIS5510 is awesome!", true, "pwn, rev");
   read_buffer();
   return 0;
 }
