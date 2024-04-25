916763858f87a63f56fc22272fd49bb4a4b3f97d
sashaNull <nguyendinhcuong0512@gmail.com>
2024-04-25 00:01:15 -0400

    Finalize Chall 7

---
+++ software-security/level-7/softsec_level7.c
@@ -0,0 +1,28 @@
+// Compiled with DEP enabled.
+// It's OK if your program crashes after the user exits the shell.
+// gcc ./softsec_level7.c -o softsec_level7 -fno-PIE -no-pie -fno-stack-protector
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void cis551_greeting(void) {
+  system("echo CIS551 is awesome!");
+}
+
+void copy_buffer(char *arg) {
+  char buf[13];
+  strcpy(buf, arg);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Error: Need a command-line argument\n");
+    return 1;
+  }
+  setuid(0);
+  cis551_greeting();
+  copy_buffer(argv[1]);
+  return 0;
+}
